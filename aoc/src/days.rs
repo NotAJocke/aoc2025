f50f8faf4bcff94eaa1e5cc6643abfb97770d70a
NotAJocke <jockerider199@gmail.com>
2025-12-01 09:08:09 +0100

    day 1

--- aoc/src/days.rs
+++ aoc/src/days.rs
@@ -3,11 +3,13 @@ use std::{collections::HashMap, sync::LazyLock};
 use crate::Day;
 
 mod day00;
+mod day01;
 
 pub static DAYS: LazyLock<HashMap<u8, Box<dyn Day + Send + Sync>>> = LazyLock::new(|| {
     let mut map = HashMap::new();
 
     map.insert(0, Box::new(crate::days::day00::Day00) as Box<_>);
+    map.insert(1, Box::new(crate::days::day01::Day01) as Box<_>);
 
     map
 });
---
+++ aoc/src/days/day01.rs
@@ -0,0 +1,123 @@
+use crate::Day;
+
+pub struct Day01;
+
+#[derive(Debug, Clone, Copy)]
+enum Rotation {
+    Left(u32),
+    Right(u32),
+}
+
+impl Rotation {
+    pub fn parse(input: &str) -> Rotation {
+        let direction = input.chars().next().unwrap();
+        let amount = input[1..].parse().unwrap();
+
+        match direction {
+            'L' => Rotation::Left(amount),
+            'R' => Rotation::Right(amount),
+            _ => unreachable!(),
+        }
+    }
+}
+
+struct Dial<C: FnMut(i32) = fn(i32)> {
+    value: i32,
+    callback: Option<C>,
+}
+
+impl<C: FnMut(i32)> Dial<C> {
+    pub fn new(initial: i32) -> Self {
+        Self {
+            value: initial,
+            callback: None,
+        }
+    }
+
+    pub fn on_click(&mut self, cb: C) {
+        self.callback = Some(cb);
+    }
+
+    pub fn apply(&mut self, rotation: Rotation) -> i32 {
+        let step = match rotation {
+            Rotation::Left(_) => -1,
+            Rotation::Right(_) => 1,
+        };
+
+        let count = match rotation {
+            Rotation::Left(n) | Rotation::Right(n) => n,
+        };
+
+        for _ in 0..count {
+            self.value = (self.value + step) % 100;
+            if let Some(cb) = &mut self.callback {
+                cb(self.value);
+            }
+        }
+
+        self.value
+    }
+}
+
+impl Day for Day01 {
+    fn part1(&self, input: &str) -> String {
+        let mut dial = Dial::<fn(i32)>::new(50);
+
+        input
+            .trim()
+            .lines()
+            .map(Rotation::parse)
+            .filter(|&r| dial.apply(r) == 0)
+            .count()
+            .to_string()
+    }
+
+    fn part2(&self, input: &str) -> String {
+        let mut dial = Dial::new(50);
+
+        let mut zeroes = 0;
+        dial.on_click(|x| {
+            if x == 0 {
+                zeroes += 1;
+            }
+        });
+
+        input
+            .trim()
+            .lines()
+            .map(Rotation::parse)
+            .for_each(|rotation| {
+                dial.apply(rotation);
+            });
+
+        zeroes.to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Day, days::day01::Day01};
+
+    const TEST: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+
+    const SOLVER: Day01 = Day01;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(SOLVER.part1(TEST), "3");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(SOLVER.part2(TEST), "6")
+    }
+}
